24b15135c498e0eb7f251a84609c7fe75a28a2cb
Sanjay Raj <sanjayraj23560@gmail.com>
2026-04-26 20:04:18 +0530

    Connected

--- frontend/src/pages/Success.jsx
+++ frontend/src/pages/Success.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react"
 import { useNavigate } from "react-router-dom"
-import introVideo from "../assets/intro.mp4"
+import introVideo from "../intro.mp4"
 
 const movies = {
   trending: [
